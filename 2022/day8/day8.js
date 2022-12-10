da3b69373d2b37760c1f766f39c854f33917766a
Daan Bergmans <bergmansdaan@live.be>
2022-12-10 17:11:14 +0100

    Finished day 8 part 2

--- 2022/day8/day8.js
+++ 2022/day8/day8.js
@@ -58,13 +58,49 @@ const checkEndOfTree = (col, treeRowIndex, treeColIndex, treeHeight, top) =>
 
 const isBorder = (treeRowIndex, treeColIndex) => treeColIndex === 0 || treeColIndex === FOREST_WIDTH - 1 || treeRowIndex === 0 || treeRowIndex === FOREST_LENGTH - 1;
 
+const checkViewForest = () => forest.map((treeRow, treeRowIndex) => checkViewTreeRow(treeRowIndex, forest));
+
+const checkViewTreeRow = (treeRowIndex, forest) => 
+    forest[treeRowIndex].map((tree, treeColIndex) => 
+        checkViewTree(
+            forest[treeRowIndex], 
+            forest.map((nTreeRow) => 
+                nTreeRow[treeColIndex]),
+            treeRowIndex,
+            treeColIndex
+        )
+    );
+
+const checkViewTree = (treeRow, treeColumn, treeRowIndex, treeColIndex) => checkViewBothSides(treeRow, treeColIndex) * checkViewBothSides(treeColumn, treeRowIndex);
+
+const checkViewBothSides = (trees, treeIndex) => checkViewOnSide(trees, treeIndex, true) * checkViewOnSide(trees, treeIndex, false);
+
+const checkViewOnSide = (trees, treeIndex, before) => {
+    if ((before === true && treeIndex - 1 === -1) || (before === false && treeIndex + 1 === trees.size)) return 0;
+
+    const treeSize = trees[treeIndex];
+
+    const sideTrees = before ? trees.slice(0, treeIndex).reverse() : trees.slice(treeIndex + 1);
+    let amountVisible = 0;
+
+    for (let i = 0; i < sideTrees.length; i++) {
+        if (sideTrees[i] >= treeSize) {
+            amountVisible++;
+            break;
+        }
+
+        amountVisible++;
+    }
+
+    return amountVisible;
+}
+
 const part1 = () => checkForest();
 
-const part2 = () => {
-};
+const part2 = () => Math.max(...checkViewForest().map((treeRow) => Math.max(...treeRow)))
 
 // Part 1
 console.log('Part 1: ', part1());
 
 // Part 2
-// console.log('Part 2: ', part2());
+console.log('Part 2: ', part2());
